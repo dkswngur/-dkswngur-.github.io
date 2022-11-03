249bd27a9585568f1a2c854b8c7501fb07820b6b
dkswngur <114394708+dkswngur@users.noreply.github.com>
2022-11-03 13:58:33 -0700

    Add files via upload

---
+++ project_2.js
@@ -0,0 +1,54 @@
+var angrySound= $("#col10_audio")[0];
+$("#col10")
+.mouseenter(function(){
+    angrySound.play();
+});
+
+var walkingSound= $("#col1_audio")[0];
+$("#col1")
+.mouseenter(function(){
+    walkingSound.play();
+});
+
+var walkingSound2= $("#col7_audio")[0];
+$("#col7")
+.mouseenter(function(){
+    walkingSound2.play();
+});
+
+var screamingSound= $("#col5_audio")[0];
+$("#col5")
+.mouseenter(function(){
+    screamingSound.play();
+});
+
+var painfulScreamingSound= $("#col14_audio")[0];
+$("#col14")
+.mouseenter(function(){
+    painfulScreamingSound.play();
+});
+
+var jumpingSound= $("#col15_audio")[0];
+$("#col15")
+.mouseenter(function(){
+    jumpingSound.play();
+});
+
+var spinningSound= $("#col30_audio")[0];
+$("#col30")
+.mouseenter(function(){
+    spinningSound.play();
+});
+
+var laughingSound= $("#col32_audio")[0];
+$("#col32")
+.mouseenter(function(){
+    laughingSound.play();
+});
+
+var laughingSound2= $("#col19_audio")[0];
+$("#col19")
+.mouseenter(function(){
+    laughingSound2.play();
+});
+
